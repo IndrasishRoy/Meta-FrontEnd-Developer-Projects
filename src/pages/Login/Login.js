e395f5e76fdb902471cc31ce390fb7339180bba5
Indrasish Roy <118255208+IndrasishRoy@users.noreply.github.com>
2023-12-30 16:03:36 +0530

    Delete src directory

--- src/pages/Login/Login.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UnderConstruction from "../Sections/UnderConstruction/UnderConstruction";
-
-const Login = () => {
-  return (
-    <UnderConstruction />
-  );
-};
-export default Login;
--- src/pages/Menu/Menu.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UnderConstruction from "../Sections/UnderConstruction/UnderConstruction";
-
-const Menu = () => {
-  return (
-    <UnderConstruction />
-  );
-};
-export default Menu;
